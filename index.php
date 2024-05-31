59d1a4eddf7949eca24a1004e3531e0059c9bda9
Surya-Gs <53792763+Surya-Gs@users.noreply.github.com>
2024-05-31 19:25:00 +0530

    Update index.php
    Added login page

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>  
 <html>  
 <head>  
-    <title>Hello World</title>  
+    <title>Login</title>  
 </head>  
 <body>  
-    <h1><?php echo "Hello, World!"; ?></h1>  
+    <h2>Login</h2>  
+    <form method="POST" action="login.php">  
+        <label for="username">Username:</label>  
+        <input type="text" id="username" name="username" required><br><br>  
+  
+        <label for="password">Password:</label>  
+        <input type="password" id="password" name="password" required><br><br>  
+  
+        <input type="submit" value="Login">  
+    </form>  
 </body>  
 </html>  
